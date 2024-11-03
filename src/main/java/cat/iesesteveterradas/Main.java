2e865eff78087a1cb827b87e6237de9922c18b33
orirribas2000@gmail.com <orirribas2000@gmail.com>
2024-11-03 22:24:08 +0100

    Metode traslladat sense problemes

--- src/main/java/cat/iesesteveterradas/Main.java
+++ src/main/java/cat/iesesteveterradas/Main.java
@@ -7,9 +7,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import static cat.iesesteveterradas.Connexio.iniciarBBDD;
 
 public class Main {
 
@@ -120,6 +123,12 @@ public class Main {
                     break;
                 case 5:
                     System.out.println("Sortint del programa");
+                    sc.close();
+                    try {
+                        con.close();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
 
             }
@@ -137,77 +146,7 @@ public class Main {
     public static List<String> readFileContent(Path filePath) throws IOException {
         return Files.readAllLines(filePath);
     }
-    public static Connection iniciarBBDD(String path){
-        // fem la llista de dades de la taula faccio;
-        ArrayList<String[]> dadesFaccio = new ArrayList<>();
-        dadesFaccio.add(new String[]{"Cavallers", "Though seen as a single group, the Knights are hardly unified. There are many Legions in Ashfeld, the most prominent being The Iron Legion."});
-        dadesFaccio.add(new String[]{"Vikings", "The Vikings are a loose coalition of hundreds of clans and tribes, the most powerful being The Warborn."});
-        dadesFaccio.add(new String[]{"Samurais","The Samurai are the most unified of the three factions, though this does not say much as the Daimyos were often battling each other for dominance."});
-        // fem la llista de dades de la taula personatge;
-        ArrayList<String[]> dadesPersonatge = new ArrayList<>();
-        dadesPersonatge.add(new String[]{"Warden","1","3","1"});
-        dadesPersonatge.add(new String[]{"Conqueror","2","2","1"});
-        dadesPersonatge.add(new String[]{"Peacekeep","2","3","1"});
 
-        dadesPersonatge.add(new String[]{"Raider","3","3","2"});
-        dadesPersonatge.add(new String[]{"Warlord","2","2","2"});
-        dadesPersonatge.add(new String[]{"Berserker","1","1","2"});
-
-        dadesPersonatge.add(new String[]{"Kensei","3","2","3"});
-        dadesPersonatge.add(new String[]{"Shugoki","2","1","3"});
-        dadesPersonatge.add(new String[]{"Orochi","3","2","3"});
-
-        //eliminem tables si existeixen
-        Connection conn = Connexio.connect(path);
-        String query="Drop table if exists faccio";
-        Connexio.queryUpdate(conn,query);
-        query="Drop table if exists personatge";
-        Connexio.queryUpdate(conn,query);
-
-        //creem les tables
-        query="Create table if not exists faccio('idFaccio' integer not null PRIMARY KEY AUTOINCREMENT,'nom' varchar(15) not null,'resum' varchar(500))";
-        Connexio.queryUpdate(conn,query);
-        query="Create table if not exists personatge('idPersonatge' integer not null PRIMARY KEY AUTOINCREMENT,'nom' varchar(15) not null," +
-                "'atac' integer(3) not null,'defensa' integer(3) not null, 'idFaccio' integer(3), foreign key (idFaccio) references faccio(id))";
-        Connexio.queryUpdate(conn,query);
-
-        //creem la query per la taula faccio
-        String queryInsertFaccio = "insert into faccio(nom,resum) values";
-        //System.out.println(queryInsertFaccio);
-        for (int i = 0; i < dadesFaccio.size(); i++) {
-            if (i!=0){
-                queryInsertFaccio+=",('%s','%s')";
-            }else {
-                queryInsertFaccio+="('%s','%s')";
-            }
-            queryInsertFaccio=queryInsertFaccio.formatted(dadesFaccio.get(i)[0],dadesFaccio.get(i)[1]);
-
-        }
-
-        Connexio.queryUpdate(conn,queryInsertFaccio);
-//        query ="Select * from faccio";
-//        Connexio.querySelect(conn,query);
-
-        //creem la query per la taula personatge
-        String queryInsertPersonatge = "insert into personatge(nom,atac,defensa,idFaccio) values";
-//        System.out.println(queryInsertPersonatge);
-        for (int i = 0; i < dadesPersonatge.size(); i++) {
-            if (i!=0){
-                queryInsertPersonatge+=",('%s',%d,%d,%d)";
-            }else {
-                queryInsertPersonatge+="('%s',%d,%d,%d)";
-            }
-            queryInsertPersonatge=queryInsertPersonatge.formatted(dadesPersonatge.get(i)[0],Integer.parseInt(dadesPersonatge.get(i)[1])
-                    ,Integer.parseInt(dadesPersonatge.get(i)[2]),Integer.parseInt(dadesPersonatge.get(i)[3]));
-
-
-        }
-
-        Connexio.queryUpdate(conn,queryInsertPersonatge);
-//        query ="Select * from personatge";
-//        Connexio.querySelect(conn,query);
-        return conn;
-    }
 
 
     public static int menu(String[] opcions){
